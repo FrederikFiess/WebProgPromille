37b761ebd997b0563344ed60503cc4111fe3d635
FR_DI <diseri.francesco@dh-karlsruhe.de>
2019-11-02 16:47:41 +0100

    Das Seekbar Element in CSS und JavaScirpt bearbeitet. Nun zeigt es die größe und höhe an.

--- Projektarbeit/app.js
+++ Projektarbeit/app.js
@@ -1,1 +1,32 @@
-//test
+window.addEventListener("load", () => {
+
+  let rangeHoehe = document.getElementById("hoehe");
+  let output = document.getElementById("laufe");
+  let rangeGewicht = document.getElementById("gewicht");
+  let output1 = document.getElementById("laufe1");
+  
+  let neuesGetraenk = document.getElementById("neuerBtn");
+  let ergebnis = document.getElementById("ergebnisBtn");
+
+  output.innerHTML = rangeHoehe.value;
+  output1.innerHTML = rangeGewicht.value;
+
+
+
+  rangeHoehe.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+  rangeGewicht.oninput = function() {
+    output1.innerHTML = this.value;
+  }
+
+
+
+
+
+
+
+
+
+});
